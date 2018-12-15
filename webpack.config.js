27c0c5bbca2affe968c1655dc8f497d5d9cc8b99
Chathan Driehuys <chathan@driehuys.com>
2018-12-15 14:20:34 -0500

    Use prettier to format code automatically.

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,52 @@
 // Built in modules
-var path = require('path');
+var path = require("path");
 
 // Plugins
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
-var DIST_DIR = path.resolve(__dirname, 'dist');
-var SOURCE_DIR = path.resolve(__dirname, 'src');
-
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SOURCE_DIR = path.resolve(__dirname, "src");
 
 // Set up Plugins
 var plugins = [];
-plugins.push(new HtmlWebpackPlugin({
-    appMountId: 'app',
+plugins.push(
+  new HtmlWebpackPlugin({
+    appMountId: "app",
     inject: false,
-    template: require('html-webpack-template'),
-}));
-
+    template: require("html-webpack-template")
+  })
+);
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(SOURCE_DIR, 'index.tsx'),
-    output: {
-        filename: "bundle.js",
-        path: DIST_DIR,
-    },
+  mode: "development",
+  entry: path.resolve(SOURCE_DIR, "index.tsx"),
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR
+  },
 
-    devServer: {
-        contentBase: DIST_DIR,
-        port: 8000
-    },
+  devServer: {
+    contentBase: DIST_DIR,
+    port: 8000
+  },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-        ]
-    },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
 
-    plugins: plugins,
+  plugins: plugins
 };
